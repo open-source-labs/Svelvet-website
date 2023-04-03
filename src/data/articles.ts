c2c0b1a16d46f43f0bda0e967364dcee40e76d54
Jen Lee <jenlee@cca.edu>
2023-04-03 06:56:08 -0700

    implemented for each loop for article links in nav

---
+++ src/data/articles.ts
@@ -0,0 +1,26 @@
+export const articles = [
+    {
+        version: 'Svelvet 1.0',
+        link: '@alexander.zambrano/simplify-application-diagramming-with-svelvet-a8f664731243/',
+    },
+    {
+        version: 'Svelvet 2.0',
+        link: 'gitconnected/svelvet-2-0-c6b2059734a6/',
+    },
+    {
+        version: 'Svelvet 4.0',
+        link: '@MauricioACastro/svelvet-4-0-the-power-of-html-is-now-inside-your-nodes-3d96823096e3/',
+    },
+    {
+        version: 'Svelvet 5.0',
+        link: '@efergus1/svelvet-5-0-a-community-driven-update-cfcc93e7b7a7/',
+    },
+    {
+        version: 'Svelvet 6.0',
+        link: '@hor.val/svelvet-6-0-the-svelte-component-library-for-building-interactive-node-based-diagrams-81dafa2d50cd/',
+    },
+    {
+        version: 'Svelvet 7.0',
+        link: 'linkGoesHere',
+    },
+]
